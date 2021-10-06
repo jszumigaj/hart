e987f98bf0ba5ed2fa50ecfc3a2c7acdea68d5b0
Jacek Szumigaj <jszumigaj.waw@aplisens.pl>
2021-10-06 21:06:22 +0200

    Use broadcast address in command #11; added unit tests

--- univrsl/Command11.go
+++ univrsl/Command11.go
@@ -1,23 +1,42 @@
 package univrsl
 
-import (
-	
-)
+import "github.com/jszumigaj/hart"
 
 // Command11 - Read unique identifier associated with TAG.
 type Command11 struct {
 	tag string
+	useBroadcastAddress bool
 	
 	// command data fields and methods inherited from Command0
 	Command0
 }
 
 // NewCommand11 creates Command 11
-func NewCommand11(device *Device, tag string) *Command11 {
+func NewCommand11(device *Device, tag string, useBroadcastAddress ...bool) *Command11 {
+	var broadcast = true
+
+	if len(useBroadcastAddress) > 0 {
+		broadcast = useBroadcastAddress[0]
+	}
+
 	return &Command11 {
 		Command0: Command0{device: device},
 		tag: tag,
+		useBroadcastAddress: broadcast,
 	}
+}
+
+func (c *Command11) DeviceId() hart.DeviceIdentifier { 
+
+	// in case of broadcast addres device identity should be cleared (all 5 bytes set zero)
+	if c.useBroadcastAddress {
+		dev := *c.device
+		dev.DevId = 0
+		dev.DevType = 0
+		dev.MfrsId = 0
+		return &dev
+	}
+	return c.device 
 }
 
 // Description property
--- univrsl/Command11_test.go
+++ univrsl/Command11_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
-
+	"github.com/jszumigaj/hart"
 	"github.com/jszumigaj/hart/status"
 	"github.com/jszumigaj/hart/univrsl"
 )
@@ -33,48 +33,85 @@ func TestCommand11SetData(t *testing.T) {
 	device := univrsl.Device{}
 	sut := univrsl.NewCommand11(&device, "tag")
 
-	
-		data := []byte{0xFE, 0xBC, 0x7B, 0x05, 0x05, 0x01, 0x01, 0x09, 0x01, 0x9D, 0x34, 0xB3, 0x05, 0x08, 0x00, 0x7C, 0x00, 0x00, 0xBC, 0x00, 0xBC, 0x01}
+	data := []byte{0xFE, 0xBC, 0x7B, 0x05, 0x05, 0x01, 0x01, 0x09, 0x01, 0x9D, 0x34, 0xB3, 0x05, 0x08, 0x00, 0x7C, 0x00, 0x00, 0xBC, 0x00, 0xBC, 0x01}
 
-		sut.SetData(data, status.NoCommandSpecificError)
-	
-		if device.ManufacturerId() != 0xbc {
-			t.Errorf("Unexpected ManId: %x", device.ManufacturerId())
+	sut.SetData(data, status.NoCommandSpecificError)
+
+	if device.ManufacturerId() != 0xbc {
+		t.Errorf("Unexpected ManId: %x", device.ManufacturerId())
+	}
+
+	if device.MfrsDeviceType() != 0x7b {
+		t.Errorf("Unexpected DevType: %x", device.MfrsDeviceType())
+	}
+
+	if device.Id() != 10302643 {
+		t.Errorf("Unexpected DevId: %x", device.Id())
+	}
+
+	if device.Preambles() != 5 {
+		t.Errorf("Unexpected preambles no: %d", device.Preambles())
+	}
+
+	if device.HartProtocolMajorRevision != 5 {
+		t.Errorf("Unexpected HartProtocolMajorRevision: %d", device.HartProtocolMajorRevision)
+	}
+
+	if device.RevisionLevel != 1 {
+		t.Errorf("Unexpected RevisionLevel: %d", device.RevisionLevel)
+	}
+
+	if device.SoftwareRevisionLevel != 1 {
+		t.Errorf("Unexpected SoftwareRevisionLevel: %d", device.SoftwareRevisionLevel)
+	}
+
+	if device.HardwareRevisionLevel != 1 {
+		t.Errorf("Unexpected HardwareRevisionLevel: %d", device.HardwareRevisionLevel)
+	}
+
+	if device.PhisicalSignalingCode != 1 {
+		t.Errorf("Unexpected PhisicalSignalingCode: %d", device.PhisicalSignalingCode)
+	}
+
+	if device.Flags != 1 {
+		t.Errorf("Unexpected Flags: %d", device.Flags)
+	}
+}
+
+func TestCheckBoradcastAddress(t *testing.T) {
+	device := &univrsl.Device{}
+	device.MfrsId = 0xbc
+	device.DevType = 0x7b
+	device.DevId = 0x123456
+
+	// default useBroadcastAddres = true
+	sut := univrsl.NewCommand11(device, "TAG")
+
+	frame := hart.LongFrameFactory(sut)
+	tx := frame.Buffer()
+	addr := tx[6:11]
+	for i, v := range addr {
+		if v != 0 {
+			t.Errorf("Expected all zeros in addr, but get addr[%v]=%v", i, v)
 		}
-		
-		if device.MfrsDeviceType() != 0x7b {
-			t.Errorf("Unexpected DevType: %x", device.MfrsDeviceType())
+	}
+}
+
+func TestCheckLongAddress(t *testing.T) {
+	device := &univrsl.Device{}
+	device.MfrsId = 0xbc
+	device.DevType = 0x7b
+	device.DevId = 0x123456
+
+	// useBroadcastAddres = false
+	sut := univrsl.NewCommand11(device, "TAG", false)
+
+	frame := hart.LongFrameFactory(sut)
+	tx := frame.Buffer()
+	addr := tx[6:11]
+	for i, v := range addr {
+		if v == 0 {
+			t.Errorf("Not expected zeros in addr, but get addr[%v]=%v", i, v)
 		}
-	
-		if device.Id() != 10302643 {
-			t.Errorf("Unexpected DevId: %x", device.Id())
-		}
-	
-		if device.Preambles() != 5 {
-			t.Errorf("Unexpected preambles no: %d", device.Preambles())
-		}
-	
-		if device.HartProtocolMajorRevision != 5 {
-			t.Errorf("Unexpected HartProtocolMajorRevision: %d", device.HartProtocolMajorRevision)
-		}
-	
-		if device.RevisionLevel != 1 {
-			t.Errorf("Unexpected RevisionLevel: %d", device.RevisionLevel)
-		}
-	
-		if device.SoftwareRevisionLevel != 1 {
-			t.Errorf("Unexpected SoftwareRevisionLevel: %d", device.SoftwareRevisionLevel)
-		}
-	
-		if device.HardwareRevisionLevel != 1 {
-			t.Errorf("Unexpected HardwareRevisionLevel: %d", device.HardwareRevisionLevel)
-		}
-	
-		if device.PhisicalSignalingCode != 1 {
-			t.Errorf("Unexpected PhisicalSignalingCode: %d", device.PhisicalSignalingCode)
-		}
-	
-		if device.Flags != 1 {
-			t.Errorf("Unexpected Flags: %d", device.Flags)
-		}
+	}
 }
